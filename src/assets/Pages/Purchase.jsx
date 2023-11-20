a64cb06a42a6162cdc6a8e06d1e31728bbf712b6
mehran <mehranasadition@gmail.com>
2023-11-20 12:23:59 +0330

    last change sth with webstorm

--- src/assets/Pages/Purchase.jsx
+++ src/assets/Pages/Purchase.jsx
@@ -1,44 +1,49 @@
-import React, { useContext, useState } from "react";
-import { FlightContext } from "../context/FlightContext";
+import React, {useContext, useState} from "react";
+import {FlightContext} from "../context/FlightContext";
 import FlightIcon from "../Ui/FlightIcon";
-import { useNavigate } from "react-router-dom";
-import { IoMdPeople } from "react-icons/io";
-import { getDateFormat, separate } from "../utils/helpers";
+import {useNavigate} from "react-router-dom";
+import {IoMdPeople} from "react-icons/io";
+import {getDateFormat, separate} from "../utils/helpers";
 import Timer from "../Ui/Timer";
 import Loader from "../Ui/Loader";
 import FormExtraPassenger from "../Ui/FormExtraPassenger";
-import { LuPlane } from "react-icons/lu";
-import { TbMoneybag } from "react-icons/tb";
-import { IoMdTime } from "react-icons/io";
-import { BsInfoCircle } from "react-icons/bs";
-import { SiCriticalrole } from "react-icons/si";
-import { MdPhone } from "react-icons/md";
-import { FiPlusCircle } from "react-icons/fi";
-import { IoPersonSharp } from "react-icons/io5";
+import {LuPlane} from "react-icons/lu";
+import {TbMoneybag} from "react-icons/tb";
+import {IoMdTime} from "react-icons/io";
+import {BsInfoCircle} from "react-icons/bs";
+import {SiCriticalrole} from "react-icons/si";
+import {MdPhone} from "react-icons/md";
+import {FiPlusCircle} from "react-icons/fi";
+import {IoPersonSharp} from "react-icons/io5";
+import {RightSideReserve} from "../Ui/RightSideReserve.jsx";
+import {LeftSideReserve} from "../Ui/LeftSideReserve.jsx";
 
 export default function Purchase() {
-  const { selectedFlight, loading } = useContext(FlightContext);
-  const [formId, setFormId] = useState([0]);
-  function addForm() {
-    const newFormId = formId[formId.length - 1] + 1;
-    setFormId([...formId, newFormId]);
-  }
-  console.log(selectedFlight);
-  const navigate = useNavigate();
+    const {selectedFlight, loading} = useContext(FlightContext);
+    const [formId, setFormId] = useState([0]);
 
-  const todayFa = {
-    day: getDateFormat(new Date(selectedFlight.departureDate), { day: "2-digit" }),
-    month: getDateFormat(new Date(selectedFlight.departureDate), { month: "numeric" }),
-    monthTitle: getDateFormat(new Date(selectedFlight.departureDate), { month: "long" }),
-    year: getDateFormat(new Date(selectedFlight.departureDate), { year: "numeric" }),
-    dayWeek: getDateFormat(new Date(selectedFlight.departureDate), { weekday: "long" }),
-  };
+    function addForm() {
+        const newFormId = formId[formId.length - 1] + 1;
+        setFormId([...formId, newFormId]);
+    }
 
-  return (
-    <>
-      {loading && <Loader />}
-      <div className="flex flex-col items-center gap-5 px-32 2xl:px-72 ">
-        {/* <div className="my-10 flex h-[220px] w-full justify-between rounded-lg bg-white ring-1 ring-gray-300">
+    console.log(selectedFlight);
+    const navigate = useNavigate();
+
+    const todayFa = {
+        day: getDateFormat(new Date(selectedFlight.departureDate), {day: "2-digit"}),
+        month: getDateFormat(new Date(selectedFlight.departureDate), {month: "numeric"}),
+        monthTitle: getDateFormat(new Date(selectedFlight.departureDate), {month: "long"}),
+        year: getDateFormat(new Date(selectedFlight.departureDate), {year: "numeric"}),
+        dayWeek: getDateFormat(new Date(selectedFlight.departureDate), {weekday: "long"}),
+    };
+
+    return (
+        <>
+            {loading && <Loader/>}
+            <div className="flex flex-col items-center gap-5 px-32 2xl:px-72 ">
+                {/*last template*/}
+                {/* <div className="my-10 flex h-[220px] w-full justify-between rounded-lg bg-white ring-1 ring-gray-300">
           <div className="flex flex-col gap-3 p-3 px-6">
             <div className="flex items-center gap-3">
               <p className="rounded-full bg-purple-100 px-4 py-1 text-sm text-primary">
@@ -143,78 +148,11 @@ export default function Purchase() {
           </div>
         </div> */}
 
-        <div className="popup rounded-lg w-[1150px] my-40 p-7">
-            <div className="grid grid-col-9 gap-5">
-              <div className="col-span-1 text-white rounded-md ring-4 ring-white">
-                <div className="flex bg-darkPrimary text-white items-center justify-between p-2">
-                  <h1>اطلاعات پرواز</h1>
-                  <LuPlane className="text-xl" />
+                <div className="popup rounded-lg grid grid-cols-12 gap-5 w-[1150px] my-40 p-7">
+                    <RightSideReserve todayFa={todayFa} selectedFlight={selectedFlight}/>
+                    <LeftSideReserve/>
                 </div>
-                <ul className="p-2">
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <TbMoneybag/>
-                    <p>مبلغ قابل پرداخت</p>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <p className="font-iranBold">{separate(selectedFlight.adultPrice)}</p>
-                    <p>{selectedFlight.currencyTitle}</p>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <IoPersonSharp/>
-                    <p>مسافر</p>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <IoMdTime/>
-                    <p>زمان باقیمانده رزرو</p>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <Timer/>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <BsInfoCircle/>
-                    <p>اطلاعات پرواز</p>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <div>
-                      <p>تاریخ</p>
-                      <p>روز</p>
-                      <p>ساعت</p>
-                      <p>مبدا</p>
-                      <p>مقصد</p>
-                      <p>شماره پرواز</p>
-                      <p>ایرلاین</p>
-                    </div>
-                    <div>
-                      <p>{todayFa.year}/{todayFa.month}/{todayFa.day}</p>
-                      <p>{todayFa.dayWeek}</p>
-                      <p className="font-iranReg">{selectedFlight.departureTime}</p>
-                      <p>{selectedFlight.fromFa}</p>
-                      <p>{selectedFlight.toFa}</p>
-                      <p>{selectedFlight.flightNumber}</p>
-                      <img alt="logo" src={selectedFlight.airlineLogo}></img>
-                    </div>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <div>
-                      <SiCriticalrole/>
-                      <a>قوانین کنسلی</a>
-                    </div>
-                    <a>کلیک کنید</a>
-                  </li>
-                  <li className="flex items-center gap-2 border-b-2 border-white py-2">
-                    <MdPhone/>
-                    <p>پشتیبانی</p>
-                  </li>
-                  <li className="flex flex-col gap-1 py-2">
-                    <p>07691006118</p>
-                    <p>09027006118</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-span-8"></div>
             </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
